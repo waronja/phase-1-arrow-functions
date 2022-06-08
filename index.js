b9df8dcb48eeace5fbdf09d650f7acf52d87b11f
unknown <crazsich@gmail.com>
2022-06-09 00:33:44 +0300

    arrow functions assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// This function divides two numbers 2000 by 100 and returns 20.
+
+const divide = () => {
+  return 2000 / 100
+}
+// Square arrow function takes one parameter and multiplies it times itself
+
+const squre = (x) => {
+    return x * x
+    }
+
+// This function takes two numbers and adds them together and returns the result.
+
+const add = (a,b) => {
+        return a + b
+    }  
+ // end of add
